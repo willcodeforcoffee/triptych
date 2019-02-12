65e87f3e0d9dceab13778e26d434732146368bea
willcodeforcoffee <eric@hoffsoftcorp.com>
2019-02-12 14:11:22 -0700

    Fixed the Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, module: 'users'
   get 'home/index'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
