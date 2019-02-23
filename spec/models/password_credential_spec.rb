33ff001d3082d86c9a881a7a4316c2028c539f5f
Eric Hoff <eric@willcodeforcoffee.ca>
2019-02-23 10:04:55 -0700

    Creating a spec

--- spec/models/password_credential_spec.rb
+++ spec/models/password_credential_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PasswordCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "defaults" do
+    subject do
+      # Don't use the factory
+      cred = PasswordCredential.new
+      cred.save
+      cred
+    end
+
+    it "should be enabled" do
+      expect (subject.enabled).to be_true
+    end
+  end
 end
