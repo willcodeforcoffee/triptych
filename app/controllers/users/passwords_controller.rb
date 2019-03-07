a199429823a7a20848f731da625589d6d27419c8
Eric Hoff <eric@willcodeforcoffee.ca>
2019-03-07 06:56:56 -0700

    Disable password reset for now

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  before_action :redirect_when_disabled, :unless => -> { is_enabled? }
+
   # GET /resource/password/new
   # def new
   #   super
@@ -31,4 +33,15 @@ class Users::PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
+
+  protected
+
+  def is_enabled?
+    # TODO: Make this configurable!
+    false
+  end
+
+  def redirect_when_disabled
+    redirect_to user_session_path, :notice => "Password reset is disabled."
+  end
 end
