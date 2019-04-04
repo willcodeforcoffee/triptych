bbf0bb5cbfa299d55686883b871f7ecc927f2789
willcodeforcoffee <eric@hoffsoftcorp.com>
2019-04-04 17:24:29 -0600

    Adding basic RackAttack stuff

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,16 @@
+# https://github.com/kickstarter/rack-attack#usage
+
+# https://github.com/kickstarter/rack-attack#fail2ban
+# After 3 blocked requests in 10 minutes, block all requests from that IP for 15 minutes.
+Rack::Attack.blocklist('fail2ban pentesters') do |req|
+    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+    # so the request is blocked
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 15.minutes) do
+      # The count for the IP is incremented if the return value is truthy
+      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+
+      req.path.include?('passwd') ||
+      req.path.include?('wp-admin') ||
+      req.path.include?('wp-login')
+    end
+  end
